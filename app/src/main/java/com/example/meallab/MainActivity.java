5e68840c47ee32091fb516b8e4d102f62329d90c
Ewout Gelling <ewoutgelling1999@gmail.com>
2020-02-18 12:51:41 +0100

    unimportant changes

--- app/src/main/java/com/example/meallab/MainActivity.java
+++ app/src/main/java/com/example/meallab/MainActivity.java
@@ -4,11 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.content.Intent;
+
+import android.view.View;
+
+import android.widget.Button;
+import android.widget.TextView;
+
+import static android.view.View.*;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // ------ Actions ------
+
+    private void goToSecondActivity() {
+
+        Intent intent = new Intent(this, SecondActivity.class);
+
+        startActivity(intent);
+
     }
 }
