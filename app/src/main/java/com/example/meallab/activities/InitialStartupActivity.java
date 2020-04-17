92685e2a781f377849a8f0f1de56b887d096a52e
Ewout Gelling <ewoutgelling1999@gmail.com>
2020-04-17 14:17:42 +0200

    Alpha version 1.0

--- app/src/main/java/com/example/meallab/activities/InitialStartupActivity.java
+++ app/src/main/java/com/example/meallab/activities/InitialStartupActivity.java
@@ -10,19 +10,28 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.content.SharedPreferences;
 
 import java.util.ArrayList;
+import java.util.Collections;
+
 import android.content.Intent;
 
+import com.example.meallab.Nutrients.Nutrient;
 import com.example.meallab.R;
 import com.example.meallab.RecyclerViewAdapterToggle;
 import com.example.meallab.Spoonacular.SpoonacularDiet;
 import com.example.meallab.Spoonacular.SpoonacularIntolerance;
+import com.example.meallab.storing_data.UserPreferences;
+import com.example.meallab.Spoonacular.SpoonacularMealType;
 import com.example.meallab.storing_data.UserPreferences;
 
 
@@ -34,16 +43,25 @@ public class InitialStartupActivity extends AppCompatActivity implements View.On
     private TextView mTextView;
     private TextView mTextView2;
     private Button saveAndContinue;
-
+    private Button resetDefaults;
+    SharedPreferences sharedPreferences;
+    public boolean firstTime;
     public ArrayList<String> allergies = new ArrayList<>();
     public ArrayList<String> diets = new ArrayList<>();
-
+    public static final String mypreference = "mypref";
+    public static String firstTimeKey = "firstTimeKey";
     RecyclerViewAdapterToggle adapter;
     RecyclerViewAdapterToggle adapter1;
     public ArrayList<String> allergyNames;
     public ArrayList<String> dietNames;
     private SeekBar bar;
-    private SeekBar bar2;
+    UserPreferences userPreferences;
+    Nutrient[] microNutrients;
+    SpoonacularMealType[] mealTypes;
+    CheckBox[] mealTypeCheckBoxes;
+    CheckBox[] microNutrientCheckBoxes;
+    EditText[] microNutrientAmounts;
+
 
     private UserPreferences prefs;
 
@@ -52,25 +70,34 @@ public class InitialStartupActivity extends AppCompatActivity implements View.On
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_initial_startup);
 
+        sharedPreferences = getSharedPreferences(mypreference, Context.MODE_PRIVATE);
+        userPreferences = new UserPreferences(this);
         this.prefs = new UserPreferences(this);
 
         initRecyclerChoices();
+        microNutrients = userPreferences.getDefaultMicroNutrients();
+        mealTypes = new SpoonacularMealType[]{SpoonacularMealType.BREAKFAST,
+                SpoonacularMealType.BRUNCH_SNACK,SpoonacularMealType.LUNCH,
+                SpoonacularMealType.APPETIZER_SNACK,SpoonacularMealType.DINNER,
+                SpoonacularMealType.DESSERT};
+
+        initLinearLayouts();
 
         bar = (SeekBar) this.findViewById(R.id.seekBar);
-        bar2 = (SeekBar) this.findViewById(R.id.seekBar2);
-        mTextView = ((TextView) InitialStartupActivity.this.findViewById(R.id.cookingTimeTextView));
-        mTextView2 = ((TextView) InitialStartupActivity.this.findViewById(R.id.textView10));
-        int progressValue = 2000;
+        mTextView = ((TextView) InitialStartupActivity.this.findViewById(R.id.caloriesSelected));
+        int progressValue = sharedPreferences.getInt("caloriesDaily", 2000);
         mTextView.setText(String.format("%d",progressValue));
         int progress = progressValue/100-5;
         bar.setProgress(progress);
-        progressValue = 3;
-        mTextView2.setText(String.format("%d", progressValue));
-        progress = progressValue-1;
-        bar2.setProgress(progress);
+
+
+
+
 
         saveAndContinue = (Button) InitialStartupActivity.this.findViewById(R.id.SaveAndContinue);
         saveAndContinue.setOnClickListener(this);
+        resetDefaults = (Button) InitialStartupActivity.this.findViewById(R.id.resetPreferences);
+        resetDefaults.setOnClickListener(this);
         bar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
@@ -91,28 +118,89 @@ public class InitialStartupActivity extends AppCompatActivity implements View.On
             }
         });
 
-        bar2.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
 
-                int value = 1 + progress;
-                InitialStartupActivity.this.mTextView2.setText(String.format("%d", value));
 
+
+
+
+    }
+
+    //Initialize linear layouts that hold checkboxes for the mealtypes and micronutrients
+    private void initLinearLayouts() {
+        Nutrient[] nutrients = userPreferences.getTrackedNutrients();
+        Nutrient[] defaultMicroNutrients = userPreferences.getDefaultMicroNutrients();
+        System.out.println(nutrients.length);
+        System.out.println("REEEEEEEEEEEEEEEE");
+        for(int i = 0; i < nutrients.length;i++){
+            for(int j = 0 ; j < microNutrients.length; j++){
+                System.out.println("i = " + i + " and j : " + j);
+                if(nutrients[i].name.equals(microNutrients[j].name)){
+
+                    microNutrients[j].amountDailyTarget = nutrients[i].amountDailyTarget;
+                }
             }
+        }
 
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
+        SpoonacularMealType[] trackedMealTypes = userPreferences.getMealsPerDay();
+        LinearLayout microNutrientLayout = (LinearLayout) this.findViewById(R.id.layout_micro_nutrients);
+        LinearLayout mealTypesLayout = (LinearLayout) this.findViewById(R.id.layout_meal_selection);
+        LayoutInflater inflater = LayoutInflater.from(this);
+        LinearLayout listItem;
+        mealTypeCheckBoxes = new CheckBox[mealTypes.length];
+        microNutrientCheckBoxes = new CheckBox[microNutrients.length];
+        microNutrientAmounts = new EditText[microNutrients.length];
 
+        for(int i = 0; i < mealTypeCheckBoxes.length ; i++){
+            listItem = (LinearLayout) inflater.inflate(R.layout.mealtypes,null ,false);
+            mealTypesLayout.addView(listItem);
+            CheckBox mealType = listItem.findViewById(R.id.mealTypeSelection);
+            mealTypeCheckBoxes[i] = mealType;
+            mealType.setText(mealTypes[i].getValue());
+
+
+        }
+
+        //micronutrients
+        for(int i = 0; i < microNutrients.length;i++){
+
+
+
+            listItem = (LinearLayout) inflater.inflate(R.layout.micronutrients,null ,false);
+            microNutrientLayout.addView(listItem);
+            CheckBox microNutrientName = listItem.findViewById(R.id.checkBox2);
+            EditText microNutrientAmount = listItem.findViewById(R.id.microNutrientAmount);
+            microNutrientCheckBoxes[i] = microNutrientName;
+            microNutrientAmounts[i] = microNutrientAmount;
+            float amount = microNutrients[i].amountDailyTarget;
+
+            microNutrientAmount.setText(String.valueOf(amount));
+
+
+            microNutrientName.setText(microNutrients[i].name);
+            TextView microNutrientUnit = listItem.findViewById(R.id.micro_nutrient_unit);
+            microNutrientUnit.setText(microNutrients[i].unit);
+            TextView microNutrientRecommended = listItem.findViewById(R.id.microNutrientDailyDose);
+            microNutrientRecommended.setText("("+defaultMicroNutrients[i].amountDailyTarget+" " + microNutrients[i].unit+ ")");
+
+
+        }
+
+
+        for(int i = 0; i < trackedMealTypes.length; i++){
+            for(int j = 0 ; j < mealTypes.length; j++){
+                if(trackedMealTypes[i].getValue().equals(mealTypes[j].getValue())){
+                    mealTypeCheckBoxes[j].setChecked(true);
+                }
             }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
+        }
+        for(int i = 0; i < nutrients.length;i++){
+            for(int j = 0; j < microNutrients.length; j++){
+                if(nutrients[i].name.equals(microNutrients[j].name)){
+                    microNutrientCheckBoxes[j].setChecked(true);
+                    break;
+                }
             }
-        });
-
-
-
+        }
     }
 
     @Override
@@ -121,6 +209,9 @@ public class InitialStartupActivity extends AppCompatActivity implements View.On
             case R.id.SaveAndContinue:
                 boolean allergies[] = adapter.getChoices();
                 boolean diets[] = adapter1.getChoices();
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putBoolean(firstTimeKey,false);
+                editor.putInt("caloriesDaily",(bar.getProgress()*100 + 500));
                 this.prefs.setIsFirstTime(false);
                 // TODO: Make user of UserPreferences.
 
@@ -128,20 +219,78 @@ public class InitialStartupActivity extends AppCompatActivity implements View.On
                 //editor.putInt("mealsDaily",(bar2.getProgress()+1));
                 int count = 0;
                 for(SpoonacularIntolerance i : SpoonacularIntolerance.values()){
-                    //editor.putBoolean(i.getValue(),allergies[count]);
+                    editor.putBoolean(i.getValue(),allergies[count]);
                     System.out.println(allergies[count]);
                     count++;
                 }
                 count = 0;
 
                 for(SpoonacularDiet d : SpoonacularDiet.values()){
-                    //editor.putBoolean(d.getValue(),diets[count]);
+                    editor.putBoolean(d.getValue(),diets[count]);
                     count++;
                 }
 
-                //editor.commit();
+                ArrayList<SpoonacularMealType> mealChoices = new ArrayList<>();
+                for(int i = 0; i < mealTypeCheckBoxes.length; i++){
+                    boolean choice = mealTypeCheckBoxes[i].isChecked();
+                    if(choice){
+                        mealChoices.add(mealTypes[i]);
+                    }
+                }
+                userPreferences.setMealsPerDay( mealChoices.toArray
+                        (new SpoonacularMealType[mealChoices.size()]));
+
+
+                Nutrient[] macroNutrients = userPreferences.getDefaultMacroNutrients();
+                ArrayList<Nutrient> microNutrientChoices = new ArrayList<>();
+                Collections.addAll(microNutrientChoices, macroNutrients);
+                for (int i = 0; i < microNutrients.length;i++){
+                    Boolean choice = microNutrientCheckBoxes[i].isChecked();
+                    if(choice){
+                        try{
+                            microNutrients[i].amountDailyTarget = Float.parseFloat(microNutrientAmounts[i].getText().toString());
+
+                        } catch (Exception e){
+
+                        }
+                        microNutrientChoices.add(microNutrients[i]);
+                    }}
+                userPreferences.setTrackedNutrients(microNutrientChoices.toArray
+                        (new Nutrient[microNutrientChoices.size()]));
+
+
+                editor.commit();
                 System.out.println("clicked in second activity");
                 goToSecondActivity();
+                break;
+
+
+            case R.id.resetPreferences:
+                Nutrient[] defaultPreferences = new Nutrient[]{};
+                userPreferences.setTrackedNutrients(defaultPreferences);
+                allergies = adapter.getChoices();
+                diets = adapter1.getChoices();
+                editor = sharedPreferences.edit();
+                editor.putBoolean(firstTimeKey,true);
+                editor.putInt("caloriesDaily",2000);
+                count = 0;
+                for(SpoonacularIntolerance i : SpoonacularIntolerance.values()){
+                    editor.putBoolean(i.getValue(),false);
+                    count++;
+                }
+                count = 0;
+
+                for(SpoonacularDiet d : SpoonacularDiet.values()){
+                    editor.putBoolean(d.getValue(),false);
+                    count++;
+                }
+
+                userPreferences.setMealsPerDay( new SpoonacularMealType[]{});
+                editor.commit();
+
+                Intent intent = new Intent(this, InitialStartupActivity.class);
+                startActivity(intent);
+                break;
 
 
         }
@@ -187,5 +336,3 @@ public class InitialStartupActivity extends AppCompatActivity implements View.On
         startActivity(intent);
     }
 }
-
-
